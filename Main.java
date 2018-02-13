9f0088763027378db85fd8efd49dfcfdd88681fb
avachandler <36450103+avachandler@users.noreply.github.com>
2018-02-13 11:27:11 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int hexidecimalAnswer = 0;
+        long decimal = 0;
+
+        System.out.print("Enter a hexadecimal number: ");
+        String hex = scanner.next();
+        hex = hex.toLowerCase();
+
+        if ( hex.substring(0,2).equals("0x")) {
+            hex = hex.substring(2);
+        }
+        for (int i = 0; i < hex.length(); i ++) {
+            char myChar = hex.charAt(i);
+            int num = 0;
+            if (myChar >= 48 && myChar <= 57) {
+                num = myChar - 48;
+            } else {
+                num = myChar - 87;
+            }
+            int pow = hex.length() - i - 1;
+            decimal += num * Math.pow(16, pow);
+        }
+            System.out.println("Your number is " + decimal + " in decimal");
+
+        }
+
+
+    }
+
+
